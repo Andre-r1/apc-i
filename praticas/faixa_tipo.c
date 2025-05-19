8b533ed7d84245b6656f728c070cd5e0f2aacb2c
André Souza Mattos <andrexpsouza@gmail.com>
2025-05-19 14:19:57 -0300

    mdc matematica discreta

---
+++ praticas/faixa_tipo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+printf("O tipo 'char' aceita valores entre %i e %i.\n" CHAR_MIN, CHAR_MAX);
+printf("O tipo 'char' aceita valores entre %i e %i.\n" CHAR_MIN, CHAR_MAX);
+
+
+int main(){
+
+    return 0;
+}
