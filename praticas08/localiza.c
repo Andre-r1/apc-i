edd205245f2cf7916182d63ecce331083bfafe2b
André Souza Mattos <andrexpsouza@gmail.com>
2025-05-21 10:54:20 -0300

    Adicionando praticas08

---
+++ praticas08/localiza.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int numero = 0;
+int achou = 0;
+for(int i=0; i<10; i++) {
+    printf("");
+    scanf("%i")
+if (numeros[i] == numero) {
+    achou = i;
+}
+}
+    scanf("%i")
+
+    return 0;
+}
